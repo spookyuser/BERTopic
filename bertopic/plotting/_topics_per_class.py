077431b58f9acff6c78a667ee27e429fab23f016
spookyuser <spookyuser@users.noreply.github.com>
2022-11-02 18:22:06 +0200

    WIP Percentage again

--- bertopic/plotting/_topics_per_class.py
+++ bertopic/plotting/_topics_per_class.py
@@ -1,19 +1,22 @@
-import pandas as pd
 from typing import List
+
+import pandas as pd
 import plotly.graph_objects as go
 from sklearn.preprocessing import normalize
 
 
-def visualize_topics_per_class(topic_model,
-                               topics_per_class: pd.DataFrame,
-                               as_percentage: bool = False,
-                               top_n_topics: int = 10,
-                               topics: List[int] = None,
-                               normalize_frequency: bool = False,
-                               custom_labels: bool = False,
-                               width: int = 1250,
-                               height: int = 900) -> go.Figure:
-    """ Visualize topics per class
+def visualize_topics_per_class(
+    topic_model,
+    topics_per_class: pd.DataFrame,
+    as_percentage: bool = False,
+    top_n_topics: int = 10,
+    topics: List[int] = None,
+    normalize_frequency: bool = False,
+    custom_labels: bool = False,
+    width: int = 1250,
+    height: int = 900,
+) -> go.Figure:
+    """Visualize topics per class
 
     Arguments:
         topic_model: A fitted BERTopic instance.
@@ -22,7 +25,7 @@ def visualize_topics_per_class(topic_model,
         top_n_topics: To visualize the most frequent topics instead of all
         topics: Select which topics you would like to be visualized
         normalize_frequency: Whether to normalize each topic's frequency individually
-        custom_labels: Whether to use custom topic labels that were defined using 
+        custom_labels: Whether to use custom topic labels that were defined using
                        `topic_model.set_topic_labels`.
         width: The width of the figure.
         height: The height of the figure.
@@ -48,7 +51,15 @@ def visualize_topics_per_class(topic_model,
     <iframe src="../../getting_started/visualization/topics_per_class.html"
     style="width:1400px; height: 1000px; border: 0px;""></iframe>
     """
-    colors = ["#E69F00", "#56B4E9", "#009E73", "#F0E442", "#D55E00", "#0072B2", "#CC79A7"]
+    colors = [
+        "#E69F00",
+        "#56B4E9",
+        "#009E73",
+        "#F0E442",
+        "#D55E00",
+        "#0072B2",
+        "#CC79A7",
+    ]
 
     # Select topics based on top_n and topics args
     freq_df = topic_model.get_topic_freq()
@@ -62,12 +73,22 @@ def visualize_topics_per_class(topic_model,
 
     # Prepare data
     if topic_model.custom_labels_ is not None and custom_labels:
-        topic_names = {key: topic_model.custom_labels_[key + topic_model._outliers] for key, _ in topic_model.topic_labels_.items()}
+        topic_names = {
+            key: topic_model.custom_labels_[key + topic_model._outliers]
+            for key, _ in topic_model.topic_labels_.items()
+        }
     else:
-        topic_names = {key: value[:40] + "..." if len(value) > 40 else value
-                       for key, value in topic_model.topic_labels_.items()}
+        topic_names = {
+            key: value[:40] + "..." if len(value) > 40 else value
+            for key, value in topic_model.topic_labels_.items()
+        }
     topics_per_class["Name"] = topics_per_class.Topic.map(topic_names)
     data = topics_per_class.loc[topics_per_class.Topic.isin(selected_topics), :]
+    global_denomenator = (
+        data.pivot_table(index=["Class"], aggfunc="sum")
+        .sort_values(by="Frequency", ascending=False)
+        .reset_index()
+    )
 
     # Add traces
     fig = go.Figure()
@@ -79,23 +100,39 @@ def visualize_topics_per_class(topic_model,
         trace_data = data.loc[data.Topic == topic, :]
         topic_name = trace_data.Name.values[0]
         words = trace_data.Words.values
+        print(trace_data.Class)
         if normalize_frequency:
             x = normalize(trace_data.Frequency.values.reshape(1, -1))[0]
             xaxis_title = "Normalized Frequency"
         if as_percentage:
-            x = trace_data.Frequency.values / trace_data.Frequency.sum()
-            xaxis_title = "Frequency (%)" 
+            # breakpoint()
+            # denomenator = data.pivot_table(index=["Class"], aggfunc="sum").sort_values(by="Frequency", ascending=False)
+            x = []
+            for index, class_name in enumerate(trace_data.Class):
+                numerator = trace_data.loc[
+                    trace_data.Class == class_name, "Frequency"
+                ].values[0]
+                denomenator = global_denomenator.loc[
+                    global_denomenator.Class == class_name, "Frequency"
+                ].values[0]
+                x.append(numerator / denomenator)
+
+            xaxis_title = "Frequency (%)"
         else:
             x = trace_data.Frequency
             xaxis_title = "Frequency"
-        fig.add_trace(go.Bar(y=trace_data.Class,
-                             x=x,
-                             visible=visible,
-                             marker_color=colors[index % 7],
-                             hoverinfo="text",
-                             name=topic_name,
-                             orientation="h",
-                             hovertext=[f'<b>Topic {topic}</b><br>Words: {word}' for word in words]))
+        fig.add_trace(
+            go.Bar(
+                y=trace_data.Class,
+                x=x,
+                visible=visible,
+                marker_color=colors[index % 7],
+                hoverinfo="text",
+                orientation="h",
+                name=topic_name,
+                hovertext=[f"<b>Topic {topic}</b><br>Words: {word}" for word in words],
+            )
+        )
 
     # Styling of the visualization
     fig.update_xaxes(showgrid=True)
@@ -104,14 +141,12 @@ def visualize_topics_per_class(topic_model,
         xaxis_title=xaxis_title,
         yaxis_title="Class",
         title={
-            'text': "<b>Topics per Class",
-            'y': .95,
-            'x': 0.40,
-            'xanchor': 'center',
-            'yanchor': 'top',
-            'font': dict(
-                size=22,
-                color="Black")
+            "text": "<b>Topics per Class",
+            "y": 0.95,
+            "x": 0.40,
+            "xanchor": "center",
+            "yanchor": "top",
+            "font": dict(size=22, color="Black"),
         },
         template="simple_white",
         width=width,
@@ -119,13 +154,9 @@ def visualize_topics_per_class(topic_model,
         xaxis=dict(
             tickformat=".0%" if as_percentage else "",
         ),
-        hoverlabel=dict(
-            bgcolor="white",
-            font_size=16,
-            font_family="Rockwell"
-        ),
+        hoverlabel=dict(bgcolor="white", font_size=16, font_family="Rockwell"),
         legend=dict(
             title="<b>Global Topic Representation",
-        )
+        ),
     )
     return fig
